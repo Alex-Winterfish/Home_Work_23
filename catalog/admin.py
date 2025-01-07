dde1f5eb14f40f627d48178d9bc38cdbc82d90fb
Алексей Булгаков <Alksbulgakov@gmail.com>
2025-01-07 20:28:19 +0300

    начаьа работа над админкой, зерегистрированный модели

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Product)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id','category_name')
+    list_filter = ('category_name')
+    search_fields = ('category_name')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('')
