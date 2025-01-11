f47847d7b211ade37c1e8b443a71f564ee83efbb
Алексей Булгаков <Alksbulgakov@gmail.com>
2025-01-11 20:57:05 +0300

    написан контролер для отображения страницы с товаром, начата работа над шаблонами

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
+from . import views
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-
+    path('product_info/', views.product_info, name='product_info')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+
 
 # Create your views here.
+
+
+def product_info(request):
+    product = Product.objects.get(id=25)
+    context = {
+        'product_name': product.product_name,
+        'product_description': product.product_description,
+        'product_price': product.product_price,
+    }
+    return render(request, 'product_info.html', context)
