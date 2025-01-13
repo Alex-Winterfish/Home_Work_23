8cce8521afdb5f42b37bf72b404e1c907ca9b23c
Алексей Булгаков <Alksbulgakov@gmail.com>
2025-01-13 21:38:40 +0300

    написан файл README.md

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('product_info/<int:product_id>/', views.product_info, name='product_info'),
-    path('product_list/', views.product_list, name='product_list'),
-    path('add_product/', views.add_product, name='add_product'),
-    path('contacts/', views.contacts, name='contacts')
+    path("product_info/<int:product_id>/", views.product_info, name="product_info"),
+    path("product_list/", views.product_list, name="product_list"),
+    path("add_product/", views.add_product, name="add_product"),
+    path("contacts/", views.contacts, name="contacts"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -9,33 +9,37 @@ from .models import Product, Category
 def product_info(request, product_id):
     product = Product.objects.get(id=product_id)
     context = {
-        'product_name': product.product_name,
-        'product_description': product.product_description,
-        'product_price': product.product_price,
-        'product_image': product.product_image
+        "product_name": product.product_name,
+        "product_description": product.product_description,
+        "product_price": product.product_price,
+        "product_image": product.product_image,
     }
-    return render(request, 'product_info.html', context)
+    return render(request, "product_info.html", context)
+
 
 def product_list(request):
     products = Product.objects.all()
-    context = {
-        'products': products
-    }
-    return render(request, 'product_list.html', context)
-
+    context = {"products": products}
+    return render(request, "product_list.html", context)
 
 
 def add_product(request):
-    if request.method == "POST":
-        name = request.POST.get('product_name')
-        description = request.POST.get('product_description')
-        price = request.POST.get('product_price')
-        prod_category = request.POST.get('category_name')
-        category = Category.objects.get(category_name=prod_category)
-        product = Product.objects.create(product_name=name, product_description=description,
-                                         product_price=price, category_name=category)
 
-    return render(request, 'add_product.html')
+    if request.method == "POST":
+        name = request.POST.get("product_name")
+        description = request.POST.get("product_description")
+        price = request.POST.get("product_price")
+        prod_category = request.POST.get("category_name")
+        category = Category.objects.get(category_name=prod_category)
+        product = Product.objects.create(
+            product_name=name,
+            product_description=description,
+            product_price=price,
+            category_name=category,
+        )
+
+    return render(request, "add_product.html")
+
 
 def contacts(request):
-    return render(request, 'contacts.html')
+    return render(request, "contacts.html")
