684261b64e21f0df324629bbea0fd456a9acf453
Алексей Булгаков <Alksbulgakov@gmail.com>
2025-01-18 17:44:04 +0300

    прдеставления написаны через cbv, начата работа над приложением blog

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
 from . import views
-from .views import ProductListView
+from .views import ProductListView, ProductDetailView, ProductCreateView, ContactView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("product_info/<int:product_id>/", views.product_info, name="product_info"),
-    path("add_product/", views.add_product, name="add_product"),
-    path("contacts/", views.contacts, name="contacts"),
-    path('product/', ProductListView.as_view(), name='product_list')
+    path("product_detail/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
+    path("product_create/", ProductCreateView.as_view(), name="product_create"),
+    path("contacts/", ContactView.as_view(template_name='catalog/contacts.html'), name="contacts"),
+    path('products/', ProductListView.as_view(), name='product_list')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,52 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic import DetailView, ListView
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, ListView, CreateView, TemplateView
 from .models import Product, Category
 
 
 # Create your views here.
 
 
-def product_info(request, product_id):
-    product = Product.objects.get(id=product_id)
-    context = {
-        "product_name": product.product_name,
-        "product_description": product.product_description,
-        "product_price": product.product_price,
-        "product_image": product.product_image,
-        "category_name": product.category_name
-    }
-    return render(request, "product_info.html", context)
-
-
 class ProductListView(ListView):
     model = Product
 
 
+class ProductDetailView(DetailView):
+    model = Product
 
-'''def product_list(request):
-    products = Product.objects.all()
-    context = {"products": products}
-    return render(request, "product_list.html", context)'''
-
-
-def add_product(request):
-
-    if request.method == "POST":
-        name = request.POST.get("product_name")
-        description = request.POST.get("product_description")
-        price = request.POST.get("product_price")
-        prod_category = request.POST.get("category_name")
-        category = Category.objects.get(category_name=prod_category)
-        product = Product.objects.create(
-            product_name=name,
-            product_description=description,
-            product_price=price,
-            category_name=category,
-        )
-
-    return render(request, "add_product.html")
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ['product_name', 'product_description', 'product_price', 'category_name']
+    success_url = reverse_lazy('catalog:product_list')
 
 
-def contacts(request):
-    return render(request, "contacts.html")
+class ContactView(TemplateView):
+    success_url = reverse_lazy('catalog:contacts')
+
