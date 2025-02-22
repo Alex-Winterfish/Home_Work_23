d9150a29b8f85804c14e3a74f4101105c8caa44f
Алексей Булгаков <Alksbulgakov@gmail.com>
2025-02-22 21:27:23 +0300

    выполнено задание 1 и часть задания 2

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,9 @@
+# -*- coding: utf-8 -*-
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseForbidden
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
+from django.views import View
 from django.views.generic import (
     DetailView,
     ListView,
@@ -9,7 +13,7 @@ from django.views.generic import (
     DeleteView,
 )
 from .models import Product
-from .forms import ProductForm
+from .forms import ProductForm, ModeratorProductForm
 
 
 class ProductListView(ListView):
@@ -26,12 +30,25 @@ class ProductCreateView(LoginRequiredMixin, CreateView):
     success_url = reverse_lazy("catalog:product_list")
     login_url = reverse_lazy("catalog:product_list")
 
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        
+        return super().form_valid(form)
+
+
 
 class ProductUpdateView(LoginRequiredMixin, UpdateView):
     model = Product
     form_class = ProductForm
     success_url = reverse_lazy("catalog:product_list")
     login_url = reverse_lazy("catalog:product_list")
+
+    def get_form_class(self):
+        user = self.request.user
+        if user.has_perm("can_unpublish_product"):
+            return ModeratorProductForm
+        else:
+            return ProductForm
 
 
 class ProductDelete(LoginRequiredMixin, DeleteView):
@@ -43,3 +60,16 @@ class ProductDelete(LoginRequiredMixin, DeleteView):
 class ContactView(TemplateView):
     success_url = reverse_lazy("blog:blog_contacts")
     login_url = reverse_lazy("catalog:product_list")
+
+
+class PublishProductView(LoginRequiredMixin, View):
+    def post(self, request, product_id):
+        product = get_object_or_404(Product, id=product_id)
+
+        if not request.user.has_perm("can_unpublish_product"):
+            return HttpResponseForbidden("У вас нет прав доступа для публикации продукта")
+
+        product.publish_attribute = True
+        product.save()
+
+        return redirect("products:product_list")
